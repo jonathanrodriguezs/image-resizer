4c58f1d55bcb26befd72718df47dc4543ba6692b
Jonathan Rodriguez Sanchez <jonathan.rodriguezs@softtek.com>
2020-09-02 01:04:12 -0500

    feat: save icons pack

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+from os import startfile
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import filedialog
+from tkinter import messagebox
 from PIL import Image, ImageTk
 from image_resizer import generate_icons, crop_center_image
 
 
-def open_file_natively():
+def open_file_browser():
     files = filedialog.askopenfilenames(
         parent=window, filetypes=[('images', ('.png', '.jpg', '.jpeg'))])
     try:
         image = files[0]
-        generate_icons(image, (120, 144, 152, 167, 180, 192, 512))
+        print(f"User selected image: {image}")
         image_pil = Image.open(image)
         loaded_img = crop_center_image(image_pil).resize((150, 150))
         render = ImageTk.PhotoImage(loaded_img)
@@ -18,8 +20,24 @@ def open_file_natively():
         img_preview.image = render
         img_preview.place(x=75, y=75)
 
+        tk.Button(window, text="Save pack of icons generated",
+                  command=lambda: save_icons_pack(image)).pack(fill='x')
     except IndexError:
         print("No file selected")
+
+
+def save_icons_pack(image, ):
+    directory = filedialog.askdirectory()
+    if directory:
+        print(f"User selected directory: {directory}")
+        # suggested sizes: (120, 144, 152, 167, 180, 192, 512)
+        generate_icons(image, directory)
+        messagebox.showinfo(
+            message="Icons were saved into " + directory, title="Success")
+        startfile(directory)
+    else:
+        messagebox.showwarning(
+            message="Please select a directory", title="Advice")
 
 
 window = tk.Tk()
@@ -28,10 +46,9 @@ window.resizable(width=False, height=False)
 window.geometry("300x300")
 
 style = ttk.Style(window)
-style.theme_use("alt")
-
+style.theme_use("clam")
 
 tk.Button(window, text="Select image",
-          command=open_file_natively).pack(fill='x')
+          command=open_file_browser).pack(fill='x')
 
 window.mainloop()
