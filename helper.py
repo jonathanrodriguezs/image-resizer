19824445f4fd4f1149267fc8ec6f488396f1c42a
Jonathan Rodriguez Sanchez <jonathan.rodriguezs@softtek.com>
2020-09-01 23:07:17 -0500

    chore: temporarily use one module

--- helper.py
+++
@@ -1,7 +0,0 @@
-def centerApp(win):
-  width = 300
-  height = 300
-  win.update_idletasks()
-  padX = (win.winfo_screenwidth() // 2) - (width // 2)
-  padY = (win.winfo_screenheight() // 2) - (height // 2)
-  win.geometry('{}x{}+{}+{}'.format(width, height, padX, padY))
