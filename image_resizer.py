00dd2ca66a777c2f6ccb0c953e218af190290a71
Jonathan Rodriguez Sanchez <jonathan.rodriguezs@softtek.com>
2020-09-01 23:01:11 -0500

    feat: image crop center

--- image_resizer.py
+++ image_resizer.py
@@ -1,23 +1,24 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import filedialog
-from PIL import Image
+from PIL import Image, ImageTk
 
 
 def resize_image(path, new_path, width, height, crop_center=True):
     '''Image resizing and saving to new path'''
     original_image = Image.open(path)
     image = original_image if not crop_center else crop_center_image(
-        path, original_image.size[1], original_image.size[1])
+        original_image)
     new_image = image.resize((width, height))
-    new_image.save("{}_{}_.{}".format(
-        new_path, str(width), original_image.format))
+    new_image.save("{}-{}.{}".format(new_path, str(width), 'png'))
 
 
-def crop_center_image(path, new_width, new_height):
-    '''Crop the center of the image'''
-    image = Image.open(path)
+def crop_center_image(image, new_width=None, new_height=None):
+    '''Crop the center of an image'''
     width, height = image.size  # Get dimensions
+
+    if (new_width is None or new_height is None):
+        new_width, new_height = height, height
 
     left = (width - new_width) / 2
     top = (height - new_height) / 2
@@ -41,19 +42,20 @@ def image_sizer():
     window.geometry("300x300")
 
     style = ttk.Style(window)
-    style.theme_use("clam")
+    style.theme_use("alt")
 
     def open_file_natively():
         files = filedialog.askopenfilenames(
-            parent=window, filetypes=[("All files", "*")])
+            parent=window, filetypes=[('images', ('.png', '.jpg', '.jpeg'))])
         try:
             image = files[0]
-            generate_icons(image)
-            # load = Image.open(image).resize((100, 100))
-            # render = ImageTk.PhotoImage(load)
-            # img = tk.Label(window, image=render)
-            # img.image = render
-            # img.place(x=0, y=0)
+            generate_icons(image, (120, 144, 152, 167, 180, 192, 512))
+            image_pil = Image.open(image)
+            loaded_img = crop_center_image(image_pil).resize((150, 150))
+            render = ImageTk.PhotoImage(loaded_img)
+            img_preview = tk.Label(window, image=render)
+            img_preview.image = render
+            img_preview.place(x=75, y=75)
 
         except IndexError:
             print("No file selected")
